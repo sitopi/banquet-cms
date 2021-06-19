fd903e9641216d2679ff3e0f4cc7cca79bdf1140
sitopi <61275085+sitopi@users.noreply.github.com>
2021-06-19 14:03:20 +0900

    viewをパーツ化

--- resources/views/drunkards/list.blade.php
+++ resources/views/drunkards/list.blade.php
@@ -1,29 +1,7 @@
-<!DOCTYPE HTML>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <title>BanQuet管理画面</title>
-    <link rel="stylesheet" href="/css/app.css">
-    <script src="/js/app.js" defer></script>
-</head>
-<body>
-    <header>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand" href="#">BanQuet管理画面</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-    <div class="navbar-nav">
-      <a class="nav-item nav-link active" href="#">酔っぱらい一覧 <span class="sr-only"></span></a>
-      <a class="nav-item nav-link" href="#">酔っぱらい追加</a>
-    </div>
-  </div>
-</nav>
-    </header>
-    <br>
-    <div class="container">
-    <div class="row">
+
+@extends('layout')
+@section('content')
+<div class="row">
   <div class="col-md-8 col-md-offset-2">
       <h2>酔っぱらい一覧</h2>
       <table class="table table-striped">
@@ -48,11 +26,4 @@
       </table>
   </div>
 </div>
-    </div>
-    <footer class="footer bg-dark  fixed-bottom">
-    <div class="container text-center">
-    <span class="text-light">©︎sitopiの階段</span>
-</div>
-    </footer>
-</body>
-</html>
+@endsection
