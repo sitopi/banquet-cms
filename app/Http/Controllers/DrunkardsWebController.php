57fc8229461b558eb2e917c82c82fb43e3b38b6d
sitopi <61275085+sitopi@users.noreply.github.com>
2021-06-19 13:34:51 +0900

    酔っぱらい一覧画面にDBから取ってきた酔っぱらいを表示する機能を追加

--- app/Http/Controllers/DrunkardsWebController.php
+++ app/Http/Controllers/DrunkardsWebController.php
@@ -3,11 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Models\Mst_drunkard;
 
 class DrunkardsWebController extends Controller
 {
     //
     public function showList(){
-        return view('drunkards.list');
+        $drunkards = Mst_drunkard::all();
+
+        // dd($drunkards);
+        return view(
+            'drunkards.list',
+            ['drunkards' => $drunkards]
+        );
     }
 }
--- resources/views/drunkards/list.blade.php
+++ resources/views/drunkards/list.blade.php
@@ -28,17 +28,23 @@
       <h2>ブログ記事一覧</h2>
       <table class="table table-striped">
           <tr>
-              <th>記事番号</th>
-              <th>日付</th>
-              <th>タイトル</th>
-              <th></th>
+              <th>酔っぱらい番号</th>
+              <th>名前</th>
+              <th>レベル</th>
+              <th>体力</th>
+              <th>攻撃力</th>
+              <th>移動速度</th>
           </tr>
+          @foreach($drunkards as $drunkard)
           <tr>
-              <td>1</td>
-              <td>2020/06/30</td>
-              <td>テスト</td>
-              <td></td>
+              <td>{{ $drunkard->drunkard_id }}</td>
+              <td>{{ $drunkard->name }}</td>
+              <td>{{ $drunkard->level }}</td>
+              <td>{{ $drunkard->hp }}</td>
+              <td>{{ $drunkard->attack }}</td>
+              <td>{{ $drunkard->move_speed }}</td>
           </tr>
+          @endforeach
       </table>
   </div>
 </div>
