d5ae5f7cfcc4510cf63f2452e39cbc85ced2fd06
sitopi <61275085+sitopi@users.noreply.github.com>
2021-06-21 09:42:39 +0900

    Fix table width in drunkard list

--- resources/views/drunkards/list.blade.php
+++ resources/views/drunkards/list.blade.php
@@ -2,7 +2,7 @@
 @extends('layout')
 @section('content')
 <div class="row">
-  <div class="col-md-8 col-md-offset-2">
+  <div class="col-md-10 col-md-offset-2">
       <h2>酔っぱらい一覧</h2>
       <table class="table table-striped">
           <tr>
